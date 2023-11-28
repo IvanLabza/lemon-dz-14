639235539ada49d01e39f5eb62d606cff675766c
IvanLabza <vanalabza77@gmail.com>
2023-11-28 11:30:36 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-function navToggle() {
-	document.getElementById("headerUl").classList.toggle("_active");
-	document.getElementById("html").classList.toggle("_overflow");
-}
+const mobileMenu = document.getElementById("menu-container");
 
+const openBtn = document.getElementById("btn-open");
+
+const closeBtn = document.getElementById("btn-close");
+
+const headElement = document.querySelectorAll("head-element");
+
+openBtn.onclick = () => {
+  mobileMenu.classList.add("_active");
+};
+
+closeBtn.onclick = () => {
+  mobileMenu.classList.remove("_active");
+};
+
+headElement.onclick = () => {
+  mobileMenu.classList.remove("_active");
+};
